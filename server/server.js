7b696f21fa05665f0bd44b7b9de20d7176f13a23
Matthew <mattwatkins000@hotmail.com>
2022-02-10 12:33:11 +1100

    finished updating lyrics

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
-//const lyricsFinder = require("lyrics-finder")
+const lyricsFinder = require("lyrics-finder")
 const SpotifyWebApi = require("spotify-web-api-node")
 
 const app = express()
